89b33be7002f01f2ecd2f96b5ab4cfc6130f7e14
JosephFuge <josephfuge1@gmail.com>
2025-02-25 14:45:33 -0700

    Fix test__read_objects.py bucket name import

--- tests/unit_tests/s3/test__read_objects.py
+++ tests/unit_tests/s3/test__read_objects.py
@@ -1,5 +1,6 @@
 from moto import mock_aws
 
+from files_api.main import S3_BUCKET_NAME as TEST_BUCKET_NAME
 from files_api.s3.read_objects import (
     fetch_s3_object,
     fetch_s3_objects_metadata,
@@ -7,7 +8,8 @@ from files_api.s3.read_objects import (
     object_exists_in_s3,
 )
 from files_api.s3.write_objects import upload_s3_object
-from tests.consts import TEST_BUCKET_NAME
+
+# from tests.consts import TEST_BUCKET_NAME
 
 
 @mock_aws
