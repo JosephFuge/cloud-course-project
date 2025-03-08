72e15413d2d90390c82573ba0b7d21be548ae3dc
JosephFuge <josephfuge1@gmail.com>
2025-03-08 00:58:15 -0700

    Fix linting with recent changes to util and route error case testing

--- src/files_api/utils.py
+++ src/files_api/utils.py
@@ -1,12 +1,14 @@
+"""Utilities file for files_api."""
 
 from typing import List
 
 
 def list_flatten(list_input: List[List]):
+    """Flatten a list of lists into just one long list."""
     result = []
 
     for ls in list_input:
         for val in ls:
             result.append(val)
-    
+
     return result
